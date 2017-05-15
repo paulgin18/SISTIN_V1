3247c37cf84b293601d9a7e778152bb30994e646
Gino Paul Gonzales Custodio <paul_3549@hotmail.com>
2017-05-15 11:07:23 -0500

    GINO 15-05-2017
    Modificaciones AÑo

--- public/js/js_general.js
+++ public/js/js_general.js
@@ -2,5 +2,24 @@ $(document).on('click', '.btnBorrar', function (event) {
     event.preventDefault();
 	var cod="";
 	$(this).parents("tr").find("#idCod").each(function(){ cod+=$(this).html();});
-	eliminar(cod);
+	bootbox.confirm({
+    title: "¿Desea Eliminar, el registro?",
+    message: "Al eliminar el registro, solo se desactivara para su uso",
+    buttons: {
+        cancel: {
+            label: '<i class="fa fa-times"></i> Cancel'
+        },
+        confirm: {
+            label: '<i class="fa fa-check"></i> Confirm'
+        }
+    },
+    callback: function (result) {
+    	if(result==true){
+			eliminar(cod);
+		}else{
+		        console.log('No se ha eliminado.');
+		    }
+    }
+});
+	
 });
