bcdfda5f2eeae278a942ba4c46f3dfc2f08c3d46
Gino Paul Gonzales Custodio <paul_3549@hotmail.com>
2017-06-05 12:16:47 -0500

    Gino
    Area

---
+++ module/Area/src/Area/Controller/AreaController.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Area\Controller;
+
+require "vendor/autoload.php";
+
+use Zend\Session\Config\SessionConfigr;
+use Zend\Session\Container;
+use Zend\Session\SessionManager;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
+use Zend\Db\Adapter\Adapter;
+use Area\Model\Entity\Area;
+use Zend\MVC\Exception;
+
+class AreaController extends AbstractActionController {
+
+	public function indexAction() {
+
+
+		//INICIO VARIABLES DE SESION
+		// $sessionConfig = new SessionConfig();
+		// $sessionConfig->setOptions(array(
+		//     'remember_me_seconds' => 180,
+		//     'use_cookies' => true,
+		//     'cookie_httponly' => true
+		//     )
+		// );
+		// $sessionManager = new SessionManager($sessionConfig);
+		// $sessionManager->start();
+		// Container::setDefaultManager($sessionManager);
+		// $session = new Container('userdata');
+		// $session->username = 'paul';
+		// $session->sis_userid = '1';
+		//FIN VARIABLES DE SESION
+//        $session = $session->username;
+//        return new ViewModel(array('session' => $session ));
+	}
+
+	public function formAction() {
+		$id = $this->params()->fromRoute("id", null);
+		$cod = $this->params()->fromRoute("cod", null);
+		if ($id !== null) {
+			if ($id == 0 ) {
+
+
+				return new ViewModel(array('mantenimiento' => 'Crear',
+					'textBoton' => 'Guardar',
+					'datos' => null));
+			} else {
+				if ($id == 1 && $cod>0) {
+				$datos = $this->buscar($cod);
+				return new ViewModel(
+						array('mantenimiento' => 'Modificar',
+					'textBoton' => 'Actualizar',
+					'datos' => $datos));
+				}
+			}
+		}
+	}
+
+	public function buscar($cod) {
+		$this->dbAdapter = $this->getServiceLocator()->get('Zend\Db\Adapter');
+		$areas = new Area($this->dbAdapter);
+		$datos = $areas->buscar($cod);
+		return $datos;
+	}
+
+	public function registrarAction() {
+		try {
+			$txtId_uni_ejec =$this->getRequest()->getPost('txtId_Uni_Ejec');
+			
+			$descripcion = $this->getRequest()->getPost('txtDescripcion');
+			$id = $this->getRequest()->getPost('txtId');
+			$this->dbAdapter = $this->getServiceLocator()->get('Zend\Db\Adapter');
+
+			$areas = new Area($this->dbAdapter);
+			if ($id != '') {
+				$modificar = $areas->modificar($id,$descripcion,$id_uni_ejec);
+				$msj = $this->mensaje($modificar, 1);
+			} else {
+				$insert = $areas->insertar($descripcion);
+				$msj = $this->mensaje($insert, 0);
+			}
+			//$msj=$this->mensaje($insert);
+						
+		} catch (\Exception $e) {
+
+
+
+			$msj = 'Error: ' . $e->getMessage();
+		}
+		$response = new JsonModel(
+				array('msj' => $msj)
+		);
+		$response->setTerminal(true);
+		return $response;
+	}
+
+
+	public function eliminarAction()
+	{
+	        $error=0;$tipoConsulta = 0;
+			
+			$id = $this->getRequest()->getPost('txtId');
+			$vigencia = $this->getRequest()->getPost('txtVigencia');
+			$this->dbAdapter = $this->getServiceLocator()->get('Zend\Db\Adapter');
+			$areas = new Area($this->dbAdapter);
+		
+		   			
+	        $eliminar = $areas->eliminar($id,$vigencia);
+			$vigencia=="false" ? $tipoConsulta=2:$tipoConsulta=3;
+			$msj = $this->mensaje($eliminar, $tipoConsulta);
+			$response = new JsonModel(array('msj' => $msj, 'error' => $error));
+			$response->setTerminal(true);
+			return $response;
+
+	}
+	public function mensaje($valorConsulta, $tipoConsulta) {
+		if ($valorConsulta == true) {
+			switch ($tipoConsulta) {
+				case 0:
+					$msj = "REGISTRADO CORRECTAMENTE";
+					break;
+				case 1:
+					$msj = "MODIFICADO CORRECTAMENTE";
+					break; 
+				case 2:
+					$msj = "ELIMINADO CORRECTAMENTE";
+					break;
+				case 3:
+					$msj = "ACTIVADO CORRECTAMENTE";
+					break;
+			}
+		} else {
+			$msj = "NO SE HA REALIZADO LA ACCION, CONSULTE CON EL ADMINISTRADOR O VUELVA A INTENTARLO";
+		}
+		return $msj;
+	}
+
+
+	public function areaAction() {
+		$this->dbAdapter = $this->getServiceLocator()->get('Zend\Db\Adapter');
+		$areas = new Area($this->dbAdapter);
+		$lista = $areas->lista();
+		$viewModel = new ViewModel(array(
+			"areas" => $lista
+		));
+		return $viewModel;
+	}
+
+
+
+}
+
+
+
+
+
---
+++ module/Area/src/Area/Model/Entity/Area.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Area\Model\Entity;
+
+use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Select;
+use Zend\Db\ResultSet\ResultSet;
+
+class Area extends TableGateway {
+
+	private $dbAdapter;
+
+	public function __construct(Adapter $adapter = null, $databaseSchema = null, ResultSet $selectResultPrototype = null) {
+		$this->dbAdapter = $adapter;
+		return parent::__construct('Area', $this->dbAdapter, $databaseSchema, $selectResultPrototype);
+	}
+
+	public function insertar($descripcion) {
+		$insert = $this->dbAdapter->
+				createStatement(
+				"INSERT INTO area (descripcion) "
+						. "VALUES ('$descripcion')");
+		$datos = $insert->execute();
+		return $datos;
+	}
+	public function modificar($id, $descripcion,$id_uni_ejec) {
+		$update = $this->dbAdapter->
+				createStatement(
+				"update area set descripcion=upper(trim('$descripcion')), id_uni_ejec=$id_uni_ejec"
+						. "where id_area=$id");
+		$datos = $update->execute();
+		return $update;
+	}
+	
+//ordenar descendente 	
+//  order by vigencia desc , decripcion asc
+
+	public function lista() {
+		//$consulta = $this->dbAdapter->query("SELECT id_estado,numero,descripcion, vigencia FROM estado order by descripcion asc", Adapter::QUERY_MODE_EXECUTE);
+
+		$consulta = $this->dbAdapter->query("SELECT descripcion,id_uni_ejec,vigencia FROM area ", Adapter::QUERY_MODE_EXECUTE);
+		$datos = $consulta->toArray();
+		return $datos;
+	}
+
+	public function buscar($id){
+        $consulta=$this->dbAdapter->query("SELECT id_area,descripcion,id_uni_ejec,vigencia FROM area where id_area=$id",Adapter::QUERY_MODE_EXECUTE);
+        $datos=$consulta->toArray();
+        return $datos[0];
+    }
+	  public function buscarArea($descripcion){
+        $consulta=$this->dbAdapter->query(
+		"SELECT id_area as value, descripcion as label,id_uni_ejec as unidadejecutora, vigencia FROM area where descripcion like '%$descripcion%'",Adapter::QUERY_MODE_EXECUTE);
+        $datos=$consulta->toArray();        
+        return $datos;    
+    }
+
+/*
+
+public function eliminar($id) {
+		$delete = $this->dbAdapter->
+				createStatement(
+				"UPDATE estado set vigencia=FALSE where id_estado=$id");
+		$datos = $delete->execute();
+		return $delete;
+	}
+*/
+   
+	public function eliminar($id,$vigencia) {
+		$delete = $this->dbAdapter->
+				createStatement("UPDATE area set vigencia=$vigencia where id_area=$id");
+		$datos = $delete->execute();
+		return $datos;
+	}
+	
+	//MARCA MODELO ??
+
+	public function buscarEstadoModelo($descripcion){
+        $consulta=$this->dbAdapter->query(
+				"SELECT id_estado as value,numero as cantidad, descripcion as label, vigencia FROM estado where descripcion like '%$descripcion%'",Adapter::QUERY_MODE_EXECUTE);
+        $datos=$consulta->toArray();        
+        return $datos;    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
